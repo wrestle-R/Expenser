77d8c7f9f329890f6d6af4ccecc83ef148557ed8
wrestle-R <russeldanielpaul@gmail.com>
2026-03-30 14:05:26 +0530

    removed native cli

--- reactnative/declarations.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "*.css";
-
-declare module '@env' {
-	export const API_URL: string | undefined;
-	export const CLERK_PUBLISHABLE_KEY: string | undefined;
-}
--- reactnative/metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
-const { withNativeWind } = require("nativewind/metro");
-
-const config = mergeConfig(getDefaultConfig(__dirname), {});
-module.exports = withNativeWind(config, {
-  input: "./global.css",
-  inlineRem: 16,         // ← this is what was missing
-});
